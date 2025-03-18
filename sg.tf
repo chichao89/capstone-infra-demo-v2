b7b903e5f9f872ca318c6cc8b3cc722ffa7506f1
naoruki <72594954+naoruki@users.noreply.github.com>
2025-03-18 16:43:48 +0800

    changes azs

--- sg.tf
+++ sg.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "group2-vpc"
   cidr = "10.0.0.0/16"
 
-  azs                = ["ap-southeast-1a", "ap-southeast-1b"]
+  azs                = ["ap-southeast-1a"]
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
   enable_nat_gateway = false
 }
