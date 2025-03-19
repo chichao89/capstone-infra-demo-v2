93b14d05a0e5a3605972abf343ca6d349f1afddd
naoruki <72594954+naoruki@users.noreply.github.com>
2025-03-19 17:03:45 +0800

    change port

--- sg.tf
+++ sg.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 5001
+    to_port     = 5001
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # ⚠️ Open to all (use your IP for security)
   }
