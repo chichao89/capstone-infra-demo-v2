91851a0f8001fcd1c26a51107f4f9b7061472af6
naoruki <72594954+naoruki@users.noreply.github.com>
2025-03-22 15:57:20 +0800

    Updated Terraform workflow for multi-environment ECS deployment

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ecs_cluster_name" {
+  description = "ECS cluster name"
+  type        = string
+}
+
+variable "ecs_task_family" {
+  description = "ECS task family"
+  type        = string
+}
+
+variable "ecs_service_name" {
+  description = "ECS service name"
+  type        = string
+}
