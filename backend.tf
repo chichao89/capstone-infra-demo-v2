6a7e21aa27f3857c2c69a0fd1c716a24ebd7323a
Javier See <70385670+chichao89@users.noreply.github.com>
2025-03-25 07:30:58 +0800

    updated workflow and backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "sctp-ce8-tfstate"
-    key     = "terraform/ce8-coaching-18/${var.environment}/terraform.tfstate"
+    key     = "terraform/ce8-coaching-18/staging/terraform.tfstate"
     region  = "ap-southeast-1"
     encrypt = true
   }
