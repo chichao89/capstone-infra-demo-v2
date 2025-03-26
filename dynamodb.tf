e1c5b83f866b78741c1c8308b193a15034caf865
naoruki <72594954+naoruki@users.noreply.github.com>
2025-03-26 14:28:20 +0800

    fixes on dynamodb

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "users_table" {
-  name         = var.dynamodb_table_name # Dynamic table name
+  name         = "${var.dynamodb_table_name}" # Dynamic table name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "username"
 
