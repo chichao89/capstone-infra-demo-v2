84187d98f012625c9e824dbd1d4da858d4f8f134
Javier See <70385670+chichao89@users.noreply.github.com>
2025-03-27 10:34:30 +0800

    m

--- ecs.tf
+++ ecs.tf
@@ -16,7 +16,8 @@ resource "aws_ecs_task_definition" "app_task" {
   container_definitions = jsonencode([
     {
       name      = var.container_name
-      image     = length(aws_ecr_repository.register_service_repo) > 0 ? "${aws_ecr_repository.register_service_repo[count.index].repository_url}:${var.environment}" # Access repository URL using count.index
+      image     = length(aws_ecr_repository.register_service_repo) > 0 ? "${aws_ecr_repository.register_service_repo[0].repository_url}:${var.environment}" : "default-image:${var.environment}"  # Use conditional logic for image
+      memory    = "512"
       memory    = "512"
       memory    = var.ecs_container_memory   # Dynamic memory for container
       cpu       = var.ecs_container_cpu      # Dynamic CPU for container
