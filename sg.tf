409fda767fedaa4b75b3f13f8bff9d35bab37fa2
naoruki <72594954+naoruki@users.noreply.github.com>
2025-04-03 22:37:02 +0800

    add azs and subnets

--- sg.tf
+++ sg.tf
@@ -4,10 +4,12 @@ module "vpc" {
   name = "group2-vpc"
   cidr = "10.0.0.0/16"
 
-  azs                = ["ap-southeast-1a"]
-  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs                = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]  # Updated AZs for 3 AZs
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]  # Updated to reflect 3 subnets for public
+  private_subnets    = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]  # Private subnets for internal resources
   enable_nat_gateway = false
 }
+
 
 # Security Group for ECS Service
 resource "aws_security_group" "ecs_sg" {
