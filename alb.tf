315479afded66a2055bfb93b300375d42485c0f0
naoruki <72594954+naoruki@users.noreply.github.com>
2025-04-03 23:12:38 +0800

    fix alb timeout

--- alb.tf
+++ alb.tf
@@ -1,12 +1,11 @@
 resource "aws_lb" "app_alb" {
-  name               = "app-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = module.vpc.public_subnets
-  enable_deletion_protection = false
+  name                          = "app-alb"
+  internal                      = false
+  load_balancer_type            = "application"
+  security_groups               = [aws_security_group.alb_sg.id]
+  subnets                       = module.vpc.public_subnets
+  enable_deletion_protection    = false
   enable_cross_zone_load_balancing = true
-  idle_timeout {
-    seconds = 60
-  }
+
+  idle_timeout_seconds = 60  # Correct attribute for idle timeout
 }
