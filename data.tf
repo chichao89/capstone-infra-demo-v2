bb4e9279b2bec9bc2dc8ec4f4e06fc3843e11382
Javier See <70385670+chichao89@users.noreply.github.com>
2025-04-06 16:00:51 +0800

    updated new readme

--- data.tf
+++ data.tf
@@ -1,17 +1,1 @@
 data "aws_caller_identity" "current" {}
-/*
-data "aws_region" "current" {}
-
-
-data "aws_vpc" "default" {
-  id = module.vpc.vpc_id
-}
-
-
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-*/
